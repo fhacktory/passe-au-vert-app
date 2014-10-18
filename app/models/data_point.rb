08d87dee0420dcee3ff5cdfdf85fa61d3615831b
Nicolas Leger <nicolas.leger@nleger.com>
2014-10-19 00:13:09 +0200

    Add DataPoint#changed_status scope

--- app/models/data_point.rb
+++ app/models/data_point.rb
@@ -6,7 +6,7 @@ class DataPoint < ActiveRecord::Base
     red_to_green: 1,
     green_to_orange: 2,
     orange_to_red: 3
-  }
+  }, scope: true
 
   validates_presence_of :traffic_light_id, :created_at, :changed_state
 
